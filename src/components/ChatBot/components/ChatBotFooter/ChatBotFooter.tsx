55611fba0a6d578bbba40b6c1427966fa20936e4
angel andres miranda castillo <mirandaangel83@gmail.com>
2024-12-19 17:45:53 -0500

    feat: handling message state and submit event

--- src/components/ChatBot/components/ChatBotFooter/ChatBotFooter.tsx
+++ src/components/ChatBot/components/ChatBotFooter/ChatBotFooter.tsx
@@ -3,22 +3,40 @@ import { Button, Form, Col, InputGroup } from 'react-bootstrap'
 import './ChatBotFooter.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { FormEvent, useState } from 'react'
 
 type Props = {}
 
 const ChatBotFooter = (props: Props) => {
-  return (
-    <div className='chat-footer'>
-        <Form className='chat-form'>
-            <InputGroup as={Col} md="8">
-                <Form.Control required type="text" placeholder="Type a message..." />
-                <Button variant="primary" type="submit">
-                    <FontAwesomeIcon icon={faArrowUp} />
-                </Button>
-            </InputGroup>
-        </Form>
-    </div>
-  )
+
+    const [message, setMessage] = useState('')
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+
+        if(message.trim() === '') return
+
+        setMessage('')
+    }
+
+    return (
+        <div className='chat-footer'>
+            <Form className='chat-form' onSubmit={handleSubmit}>
+                <InputGroup as={Col} md="8">
+                    <Form.Control 
+                        required 
+                        type="text" 
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        placeholder="Type a message..." 
+                    />
+                    <Button variant="primary" type="submit">
+                        <FontAwesomeIcon icon={faArrowUp} />
+                    </Button>
+                </InputGroup>
+            </Form>
+        </div>
+    )
 }
 
 export default ChatBotFooter
