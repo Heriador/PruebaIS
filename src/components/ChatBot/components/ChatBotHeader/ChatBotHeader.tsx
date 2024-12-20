d48eee08dfd50a56a638529e31c151a69a91753a
angel andres miranda castillo <mirandaangel83@gmail.com>
2024-12-20 11:01:12 -0500

    feat: make button popup so chatbot can be close and open when click on it

--- src/components/ChatBot/components/ChatBotHeader/ChatBotHeader.tsx
+++ src/components/ChatBot/components/ChatBotHeader/ChatBotHeader.tsx
@@ -2,16 +2,26 @@ import { Button } from "react-bootstrap"
 import ChatBotIcon from "../ChatBotIcon"
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Dispatch, SetStateAction } from "react"
 import "./ChatBotHeader.css"
 
-const ChatBotHeader = () => {
+type Props = {
+  setShowChatBot: Dispatch<SetStateAction<boolean>>
+}
+
+const ChatBotHeader = ({setShowChatBot}: Props) => {
+
+  const closeChatBot = () => {
+    setShowChatBot(false)
+  }
+
   return (
     <div className="chat_header">
       <div className="header-info">
         <ChatBotIcon/>
         <h2 className="logo-text">ChatBot</h2>
       </div>
-      <Button variant="outline-light">
+      <Button variant="primary" onClick={closeChatBot}>
         <FontAwesomeIcon icon={faChevronDown} />
       </Button>
     </div>
