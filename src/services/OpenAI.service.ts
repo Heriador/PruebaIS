82c7cc375c1d8e378f17e459ae6beddadef5ec9e
angel andres miranda castillo <mirandaangel83@gmail.com>
2024-12-20 11:09:57 -0500

    refactor: change gpt model to latest

--- src/services/OpenAI.service.ts
+++ src/services/OpenAI.service.ts
@@ -8,7 +8,7 @@ const openai = new OpenAI({
 
 const generateChatResponse = async (chatHistory: Message[], prompt: string): Promise<string> => {
     const response = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo',
+        model: 'gpt-4o',
         messages: [
             {
                 role: 'system',
